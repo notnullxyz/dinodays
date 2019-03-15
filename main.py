2f0c33e31c487122ef3d218dd13cb04f67d25cd1
Marlon van der Linde <marlonvdl@infinea.net>
2019-03-15 12:43:59 +0200

    Dir structure... and then loadshedding. Continue later

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from sys import exit
+from src.ark.ark import Ark
+
+
+def run(real_life_mins, day_game_hours, night_game_hours):
+    ark = Ark()
+    print('Ok')
+
+
+def main():
+    """ main entry point."""
+    run(1,1,1)
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ src/ark/ark.py
@@ -0,0 +1,15 @@
+"""
+Ark Specific Stuff, like the known formulas.
+"""
+
+
+class Ark:
+
+    def calc_day_cycle_speed_scale(self, real_life_minutes):
+        pass
+
+    def calc_day_time_speed_scale(self):
+        pass
+
+    def calc_night_time_speed_scale(self):
+        pass
