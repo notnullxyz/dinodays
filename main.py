b2af5943ff42b247e7fccf780c4edfbaa6da6a1f
Marlon van der Linde <marlonvdl@infinea.net>
2019-03-18 15:10:29 +0200

    Working, basically. Standard output, and json dumped output

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 
 from sys import exit
+import json
 from src.ark.ark import Ark
+from argparse import ArgumentParser
 
 
 def run(real_life_mins, day_game_hours, night_game_hours):
     ark = Ark()
-    print('Ok')
-
+    day_cycle_speed_scale = ark.calc_day_cycle_speed_scale(real_life_mins)
+    day_time_speed_scale = ark.calc_day_time_speed_scale(day_game_hours)
+    night_time_speed_scale = ark.calc_night_time_speed_scale(night_game_hours)
+    return dict(
+        day_cycle_speed_scale=day_cycle_speed_scale,
+        day_time_speed_scale=day_time_speed_scale,
+        night_time_speed_scale=night_time_speed_scale
+    )
 
 def main():
     """ main entry point."""
-    run(1,1,1)
+
+    parser = ArgumentParser(description='Calculate the configuration values for speed scales in ARK servers')
+
+    parser.add_argument('--json', default=False, action='store_true',
+                        help='output results as json')
+    parser.add_argument('desired_reallife_minutes', action='store', type=float,
+                        help='The desired real-life minutes an in-game day should last')
+
+    parser.add_argument('desired_day_hours_ingame', action='store', type=float,
+                        help='The desired length of the day (sunlight) in \'in-game hours\'')
+
+    parser.add_argument('desired_night_hours_ingame', action='store', type=float,
+                        help='The desired length of the night (dark) in \'in-game hours\'',
+                        )
+    arguments = parser.parse_args()
+    result = run(arguments.desired_reallife_minutes, arguments.desired_day_hours_ingame,
+                 arguments.desired_night_hours_ingame)
+
+    if arguments.json:
+        print(json.dumps(result))
+    else:
+        print("Day Cycle Speed Scale: " + str(result['day_cycle_speed_scale']))
+        print("Day Time Speed Scale: " + str(result['day_time_speed_scale']))
+        print("Night Time Speed Scale: " + str(result['night_time_speed_scale']))
 
 
 if __name__ == '__main__':
--- src/ark/ark.py
+++ src/ark/ark.py
@@ -1,15 +1,20 @@
 """
 Ark Specific Stuff, like the known formulas.
+Defaults were correct when this script was written.
 """
 
 
 class Ark:
 
-    def calc_day_cycle_speed_scale(self, real_life_minutes):
-        pass
+    ARK_STANDARD_DAY_MINUTES = 50.675
+    ARK_DEFAULT_DAYTIME_HOURS = 16.5
+    ARK_DEFAULT_NIGHTTIME_HOURS = 7.5
 
-    def calc_day_time_speed_scale(self):
-        pass
+    def calc_day_cycle_speed_scale(self, real_life_minutes_desired: float):
+        return self.ARK_STANDARD_DAY_MINUTES / real_life_minutes_desired
 
-    def calc_night_time_speed_scale(self):
-        pass
+    def calc_day_time_speed_scale(self, in_game_hours_desired: float):
+        return 1.0 + (1.0 - (in_game_hours_desired / self.ARK_DEFAULT_DAYTIME_HOURS))
+
+    def calc_night_time_speed_scale(self, in_game_hours_desired: float):
+        return 1.0 + (1.0 - (in_game_hours_desired / self.ARK_DEFAULT_NIGHTTIME_HOURS))
